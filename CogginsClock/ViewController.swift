fd255e26808a66831b9b92c99a13a04208387f0a
Emily Coggins <139669664+embcogg@users.noreply.github.com>
2024-02-02 12:20:46 -0600

    Initial Commit

---
+++ CogginsClock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CogginsClock
+//
+//  Created by Emily Coggins on 2/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
